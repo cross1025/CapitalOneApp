5162ebcaf19c5c32b4d995080dc293686fc23c68
Casey.Cross <ccrossaston@gmail.com>
2023-09-12 11:39:43 -0500

    Initial Commit - Created CardOffers Module

---
+++ CardOffers/src/main/java/CardOffersApplication.java
@@ -0,0 +1,5 @@
+public class CardOffersApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/capital/capitaloneapp/CapitalOneAppApplication.java
@@ -0,0 +1,13 @@
+package com.capital.capitaloneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalOneAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalOneAppApplication.class, args);
+    }
+
+}
